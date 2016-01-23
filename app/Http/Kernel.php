4a1a23910ad0279341102636e6251038900fcfdb
Chris Gmyr <cmgmyr@gmail.com>
2016-01-23 10:44:28 -0500

    patch New Relic and L5.2 issue on Forge?

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Sv\Http\Middleware\NewRelicPatch::class,
     ];
 
     /**
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,6 +4,9 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
     Route::get('soon', ['as' => 'soon', 'uses' => 'HomeController@soon']);
 
+    Route::group(['prefix' => 'clients'], function() {
+        Route::get('/', ['as' => 'clients.index', 'uses' => 'ClientController@index']);
+    });
 
     // Authentication Routes...
     $this->get('login', ['as' => 'login', 'uses' => 'Auth\AuthController@showLoginForm']);
@@ -14,4 +17,8 @@ Route::group(['middleware' => 'web'], function () {
     $this->get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
     $this->post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
     $this->post('password/reset', 'Auth\PasswordController@reset');
+});
+
+Route::macro('after', function ($callback) {
+    $this->events->listen('router.filter:after:newrelic-patch', $callback);
 });
