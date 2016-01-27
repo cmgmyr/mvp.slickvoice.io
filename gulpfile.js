c9606242ca1884e22f12329249cb97bf13588b99
Chris Gmyr <cmgmyr@gmail.com>
2016-01-27 17:19:09 -0500

    add jquery ui

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var elixir = require('laravel-elixir');
 var paths = {
     'bootstrap': 'vendor/bower_components/bootstrap/',
     'jquery': 'vendor/bower_components/jquery/dist/',
+    'jqueryui': 'vendor/bower_components/jquery-ui/',
     'fontawesome': 'vendor/bower_components/fontawesome/'
 };
 
@@ -11,13 +12,14 @@ elixir(function(mix) {
 
     .scripts([
         paths.jquery + 'jquery.js',
+        paths.jqueryui + 'jquery-ui.js',
         paths.bootstrap + 'dist/js/bootstrap.js',
         'resources/assets/js/app.js'
     ], 'public/js/app.js', './')
 
     .copy(paths.bootstrap + 'fonts/**', 'public/build/fonts')
     .copy(paths.fontawesome + 'fonts/**', 'public/build/fonts')
-
+    .copy(paths.jqueryui + 'themes/base/images/**', 'public/build/css/images')
 
     .version(['css/app.css', 'js/app.js']);
 });
