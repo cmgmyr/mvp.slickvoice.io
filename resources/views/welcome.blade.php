c2e75a0a4f4bbb7fb7fe7d5451e134dbcf12878b
Chris Gmyr <cmgmyr@gmail.com>
2016-02-12 18:33:28 -0500

    update sales welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,27 +3,24 @@
 @section('content')
     <!-- Jumbotron -->
     <div class="jumbotron">
-        <h1>Welcome to SlickVoice.io</h1>
-        <p class="lead">Invoicing and Payment Service for Freelancers and Developers.</p>
+        <h1>Invoicing and Subscription Billing</h1>
+        <p class="lead">SlickVoice.io is the fastest way to get paid online.<br> Create invoices, securely store client credit cards, and schedule reoccuring invoices in seconds.</p>
         <p><a class="btn btn-lg btn-success" href="#" role="button">Coming Soon!</a></p>
     </div>
 
     <!-- Example row of columns -->
     <div class="row">
         <div class="col-md-4">
-            <h2>Easy To Use</h2>
-            <p>Creating clients, invoices, and handling payments couldn't be easier. Using our simple user interface will get you back to what you love doing in no time (not book keeping).</p>
-            <p><a class="btn btn-primary" href="#" role="button">Coming Soon!</a></p>
+            <h2><i class="fa fa-heart"></i> Always Easy</h2>
+            <p>Creating clients, invoices, and handling payments couldn't be easier. Using our simple user interface will get you back to what you love doing in no time (not bookkeeping).</p>
         </div>
         <div class="col-md-4">
-            <h2>Get Paid</h2>
-            <p>Quickly send customer invoices, or save client credit cards securly to invoice them on a reoccuring basis.</p>
-            <p><a class="btn btn-primary" href="#" role="button">Coming Soon!</a></p>
+            <h2><i class="fa fa-money"></i> Always Convenient</h2>
+            <p>Quickly set up reoccurring, or one-time, invoices and we'll process them securely (through Stripe) on the due date. We'll notify you once completed, or if there were any issues.</p>
         </div>
         <div class="col-md-4">
-            <h2>Always Professional</h2>
-            <p>We have beautiful invoice and email templates including your own logo. Your clients will love them.</p>
-            <p><a class="btn btn-primary" href="#" role="button">Coming Soon!</a></p>
+            <h2><i class="fa fa-black-tie"></i> Always Professional</h2>
+            <p>We've handcrafted beautiful invoice and email templates that are sent automatically. You, and your clients, will love them.</p>
         </div>
     </div>
 @endsection
